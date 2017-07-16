a61ebdac65f45992a5f6484f27a10c0ce332fac1
Seif <seif@seif.rocks>
2017-07-16 20:59:24 +0300

    initial commit

---
+++ payligent/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from payligent import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+import registration.backends.simple.views
+
+app_name='payligent'
+
+urlpatterns=[
+	url(r'^$', views.index, name='index'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
